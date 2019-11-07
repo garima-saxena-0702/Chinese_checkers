10113000ff4b1f904211c0e3e1232e24a13007e9
Garima Saxena <garimasaxena0702@gmail.com>
2019-11-07 18:14:41 +0530

    Adding pegs

--- game.js
+++ game.js
@@ -1,4 +1,7 @@
 let allPositions = [];
+let redPegs = [];
+let bluePegs = [];
+let player = 'red';
 
 class Positions {
      
@@ -6,5 +9,13 @@ class Positions {
         this.x = x;
         this.y = y;
         this.color = null;
+    }
+}
+
+class Pegs {
+    constructor(position, color) {
+        this.x = position.x;
+        this.y = position.y;
+        this.color = color;
     }
 }
