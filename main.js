409c368a78bc51121ed7af632ee1f498ff5cbc90
Garima Saxena <garimasaxena0702@gmail.com>
2019-11-07 18:15:50 +0530

    added functionality for click

--- main.js
+++ main.js
@@ -1,36 +1,63 @@
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
-ctx.strokeStyle = "brown";
-const radius = 18;
-const distance = 40;
-const noOfRows = 13;
-const rowDivsion = Math.floor(13/2);
+let marked = {x: null, y: null, color: null, index: null};
+let allowedPlaces = [];
 
-function drawCircles() {
-    for(var i = 0; i < rowDivsion+2; i++){
-        for(var j = 0; j < i; j++) {
-            ctx.beginPath();
-            ctx.arc(canvas.width/2 + (distance * (j-i)) + (distance/2 * i), distance * (i), radius, 0, 2*Math.PI);
-            allPositions.push(new Positions(canvas.width/2 + (distance * (j-i)) + (distance/2 * i), distance * (i)))
-            ctx.stroke();
-        }
+function calculateAlllowedPlaces() {
+    allowedPlaces = allPositions;
+    return allowedPlaces;
+}
+
+function isEmptyObject(obj) {
+    if(Object.entries(obj).length === 0 && obj.constructor === Object) return true
+    return false
+}
+
+function checkWin(playerColor) {
+    let i, max;
+    if(playerColor == 'blue') {
+        i = 0;
+        max = 10;
     }
-    for(var k = 0; k < rowDivsion; k++) {
-        for(var j = rowDivsion; j > k; j--) {
-            ctx.beginPath();
-            ctx.arc(canvas.width/2 + (distance * (k-j)) + (distance/2 * (rowDivsion-k)), distance * (i), radius, 0, 2*Math.PI);
-            allPositions.push(new Positions(canvas.width/2 + (distance * (k-j)) + (distance/2 * (rowDivsion-k)), distance * (i)))
-            ctx.stroke();
-        }
-        i++;
+    else {
+        i = 39;
+        max = 49;
     }
-    console.log(allPositions);
+    for(; i < max; i++) {
+        if(allPositions[i].color !== playerColor) return false; 
+    }
+    let winCanvas = document.getElementById('winCanvas')
+    winCanvas.innerHTML = playerColor
+    canvas.style.display = 'none';
+    return true;
 }
 
 canvas.addEventListener('click', (event) => {
     x = event.clientX - canvas.offsetLeft;
     y= event.clientY - canvas.offsetTop;
-    console.log(x, y);
+    for(let i = 0; i < allPositions.length; i++) {
+        if((allPositions[i].x - radius < x && allPositions[i].x + radius > x) && (allPositions[i].y - radius < y && allPositions[i].y + radius > y)) {
+            if(allPositions[i].color && allPositions[i].color == player) {
+                createCircles(allPositions[i].x, allPositions[i].y, radius-1, 'rgba(0,255,0,1)');
+                createCircles(allPositions[i].x, allPositions[i].y, radius-5, allPositions[i].color);
+                if(marked.color){
+                    createCircles(marked.x, marked.y, radius-1, marked.color);  
+                } 
+                marked.x = allPositions[i].x;
+                marked.y = allPositions[i].y;
+                marked.color = allPositions[i].color;
+                marked.index = i;
+                calculateAlllowedPlaces();
+            }
+            else {
+                if(allowedPlaces.filter(place => {return place.x == allPositions[i].x && place.y == allPositions[i].y}).length && !isEmptyObject(marked)) {
+                    createCircles(marked.x, marked.y, radius-1, 'white');
+                    createCircles(allPositions[i].x, allPositions[i].y, radius-1, marked.color)
+                    allPositions[i].color = marked.color;
+                    allPositions[marked.index].color = null;
+                    checkWin(player);
+                    marked = {};
+                    player = player == 'red' ? 'blue' : 'red';
+                }
+            }
+        }
+    }  
 })
-
-drawCircles();
